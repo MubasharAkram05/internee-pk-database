384df6fd743940d31635ca213efd18463e6c2d9f
MubasharAkram05 <mubasharakram05@gmail.com>
2026-05-10 19:33:26 +0500

    Delete myproject directory

--- myproject/posts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . import models
-
-class CreatPost(forms.ModelForm):
-    class Meta:
-        model = models.Post
-        fields = ['title', 'body' , 'slug' , 'banner']
